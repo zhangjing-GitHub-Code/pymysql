678f020872405c549d83e2517673f75218b8a0c5
zhangjing-github-code <zhangjing20091105@126.com>
2020-11-04 22:31:12 +0800

    第一稿

--- dbop.py
+++ dbop.py
@@ -4,4 +4,11 @@ def getconn():
     return db
 def addtab(tablename,db):
     cur=db.cursor()
+    print("start to enter infomations."
+    id = input("enter id:")
+    name = input("enter member's name:")
+    sex = input("enter sex in Chinese (男 or 女)")
+    boda = input("enter when is this member born(for example 2016.11.5):")
+    others = input("enter others:")
+    val = (id,name,sex,boda,others)
     sql1="insert into `"+tabname+"` values"+str(val)
